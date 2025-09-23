fb99678ab0f8ef1de29a452233de7d36c434a967
zerajudeen-zera <zerajudeen@gmail.com>
2025-09-23 23:14:14 +0530

    updated

--- deploy.sh
+++ deploy.sh
@@ -40,6 +40,9 @@ echo "🚀 Starting deployment process to the Amazon EC2 server..."
 : "${KEY_PATH:?Environment variable KEY_PATH not set}"
 : "${COMPOSE_FILE:?Environment variable COMPOSE_FILE not set}"
 
+echo "🔐 Adding EC2 host key to known_hosts..."
+ssh-keyscan -H "$AMAZON_EC2_IP" >> ~/.ssh/known_hosts
+
 echo "📦 Copying the compose file from Jenkins to EC2..."
 scp -i "$KEY_PATH" "$COMPOSE_FILE" "$AMAZON_EC2_USER@$AMAZON_EC2_IP:/home/$AMAZON_EC2_USER/"
 
